98e36e27172c4ecd224906df9fe09534e3f0e670
Nicolas Quillet <nicolas.quillet@ispace-syscom.fr>
2017-09-04 17:35:20 +0200

    Add developer route to show phpinfo

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,12 @@
 <?php
 // Routes
 
+$app->get('/developer', function ($request, $response, $args) {
+
+    echo phpinfo();
+
+});
+
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
@@ -8,3 +14,5 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
