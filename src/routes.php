2194a65ca9baf9581d2804eeff27e4df50513cbe
Nicolas Quillet <nicolas.quillet@ispace-syscom.fr>
2017-09-05 16:15:39 +0200

    Test de pipeline Jenkins

--- src/routes.php
+++ src/routes.php
@@ -11,6 +11,8 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
+    // Test pipeline
+
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
